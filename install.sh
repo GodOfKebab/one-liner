42d0008e8fab6e5a9a13922c68f7cb73c2b3ea4a
Yaşar İdikut <yasar.idikut@hisarschool.k12.tr>
2022-05-08 22:44:34 -0400

    installation to be tested

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 
 echo "Setting up one-liner..."
 
-ONELINER_TEMPLATE="https://raw.githubusercontent.com/GodOfKebab/one-liner/master/src/.one-liner"
-ONELINER_PYTHON_URL="https://raw.githubusercontent.com/GodOfKebab/one-liner/master/src/one-liner.py"
+ONELINER_TEMPLATE="https://raw.githubusercontent.com/GodOfKebab/one-liner/master/.one-liner"
+ONELINER_PYTHON_URL="https://raw.githubusercontent.com/GodOfKebab/one-liner/master/one-liner.py"
+
+alias source_one_liner='source ~/.one-liner'
 
 curl -Ls $ONELINER_TEMPLATE -o ~/.one-liner
-sh ~/.one-liner
+source_one_liner
 ONELINER_FILE_CONTENTS=$(curl -Ls $ONELINER_PYTHON_URL)
 curl -Ls $ONELINER_PYTHON_URL | python3 - init --init_file_contents "$ONELINER_FILE_CONTENTS"
 
@@ -18,6 +20,6 @@ elif [ $SHELL = "/bin/zsh" ]; then
 	echo "source ~/.one-liner" >> ~/.zshrc
 fi
 
-sh ~/.one-liner
+source_one_liner
 echo "All set. You can start using one-liner."
 
