fb3b0d9aa87c05ddee203f6db16dead92b31e77a
Yaşar İdikut <yasar.idikut@hisarschool.k12.tr>
2022-05-08 23:20:58 -0400

    installation to be tested

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ ONELINER_TEMPLATE="https://raw.githubusercontent.com/GodOfKebab/one-liner/master
 ONELINER_PYTHON_URL="https://raw.githubusercontent.com/GodOfKebab/one-liner/master/one-liner.py"
 
 curl -Ls $ONELINER_TEMPLATE -o ~/.one-liner
-source "$HOME/.one-liner"
+ONELINER_PATH="$HOME/.one-liner"
+export ONELINER_PATH
 ONELINER_FILE_CONTENTS=$(curl -Ls $ONELINER_PYTHON_URL)
 curl -Ls $ONELINER_PYTHON_URL | python3 - init --init_file_contents "$ONELINER_FILE_CONTENTS"
 
